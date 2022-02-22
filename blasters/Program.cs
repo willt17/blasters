d08d0a69465e9d2fc00f2a5683fd779f19f08ced
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-02-21 20:26:27 -0600

    Started stubbing

--- blasters/Program.cs
+++ blasters/Program.cs
@@ -7,6 +7,50 @@ namespace blasters
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BlastersStart();
+        }
+        static void BlastersStart()
+        {
+            System.Console.WriteLine("Welcome to blasters.");
+            System.Console.WriteLine("Prepare to play");
+            CallShot();
+        }
+        static void CallShot()
+        {
+            System.Console.WriteLine("Master Yodas is testing you, would you like to attempt to dodge or deflect the shot?");
+            System.Console.WriteLine("Press 1 to dodge or 2 to deflect");
+            string userInput = Console.ReadLine();
+            if(userInput == "1")
+            {
+                DodgeChance();
+            }
+            else if(userInput == "2")
+            {
+                DeflectChance();
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid choice");
+            }
+        }
+        static void DodgeChance()
+        {
+            System.Console.WriteLine("Dodge chance");
+            Random rnd = new Random();
+            int result = rnd.Next(10);
+            // System.Console.WriteLine(result);
+            if(result >= 5)
+            {
+                System.Console.WriteLine("You dodged.");
+            }
+            else
+            {
+                System.Console.WriteLine("You're hit.");
+            }
+        }
+        static void DeflectChance()
+        {
+            System.Console.WriteLine("Deflect chance");
         }
     }
 }
