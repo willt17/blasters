f7d1aea706e84ec796fd79d2052df5cf4f64d404
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-02-24 09:50:03 -0600

    Undid test changes

--- blasters/Program.cs
+++ blasters/Program.cs
@@ -34,7 +34,7 @@ namespace blasters
                 }
                 else
                 {
-                    System.Console.WriteLine("test");
+                    System.Console.WriteLine("");
                 }
             }
         }
