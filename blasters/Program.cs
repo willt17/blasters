af80b2f978f655418af26fda35971ef93f6c15d7
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-02-23 21:14:56 -0600

    working points while loop

--- blasters/Program.cs
+++ blasters/Program.cs
@@ -13,11 +13,33 @@ namespace blasters
         {
             System.Console.WriteLine("Welcome to blasters.");
             System.Console.WriteLine("Prepare to play");
-            CallShot();
-        }
-        static void CallShot()
-        {
             int userPoints = 15;
+            while(userPoints > 0)
+            {
+                string tempPoints = CallShot();
+                if(tempPoints == "1")
+                {
+                    userPoints += 10;
+                    System.Console.WriteLine("You now have " + userPoints + " points.");
+                }
+                else if(tempPoints == "2")
+                {
+                    userPoints -= 5;
+                    System.Console.WriteLine("You now have " + userPoints + " points.");
+                }
+                else if(tempPoints == "3")
+                {
+                    userPoints += 10;
+                    System.Console.WriteLine("You now have " + userPoints + " points.");
+                }
+                else
+                {
+                    System.Console.WriteLine("else.");
+                }
+            }
+        }
+        static string CallShot()
+        {
             System.Console.WriteLine("Master Yodas is testing you, would you like to attempt to dodge or deflect the shot?");
             System.Console.WriteLine("Press 1 to dodge or 2 to deflect");
             string userInput = Console.ReadLine();
@@ -27,19 +49,27 @@ namespace blasters
                 if(hit == true)
                 {
                     System.Console.WriteLine("hit works.");
+                    string hitTrue = "1";
+                    return hitTrue;
                 }
                 else
                 {
                     System.Console.WriteLine("miss works.");
+                    string hitFalse = "2";
+                    return hitFalse;
                 }
             }
             else if(userInput == "2")
             {
                 DeflectChance();
+                string deflectTest = "3";
+                return deflectTest;
             }
             else
             {
                 System.Console.WriteLine("Invalid choice");
+                string invalidHit = "4";
+                return invalidHit;
             }
         }
         static bool DodgeChance()
